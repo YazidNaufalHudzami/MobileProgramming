035c1bf74631692613a1cfd82cd8a8a815c8a4c1
YazidNaufalHudzami <41522010078@student.mercubuana.ac.id>
2024-04-20 11:37:02 +0700

    finished tugas 2 project

--- app/src/main/java/com/example/tugas2_yazidnaufalhudzami_41522010078/login.java
+++ app/src/main/java/com/example/tugas2_yazidnaufalhudzami_41522010078/login.java
@@ -10,29 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class loginorregister extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_loginorregister);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    public void toLogin(View view) {
-        Intent login = new
-                Intent(loginorregister.this, login.class);
-        startActivity(login);
+    public void tologinorregister(View view) {
+        Intent loginorregister = new
+                Intent(login.this, loginorregister.class);
+        startActivity(loginorregister);
     }
 
-    public void toRegister(View view) {
-        Intent register = new
-                Intent(loginorregister.this, register.class);
-        startActivity(register);
+    public void toBeranda(View view) {
+        Intent beranda = new
+                Intent(login.this, beranda.class);
+        startActivity(beranda);
     }
 }
--- app/src/main/java/com/example/tugas2_yazidnaufalhudzami_41522010078/register.java
+++ app/src/main/java/com/example/tugas2_yazidnaufalhudzami_41522010078/register.java
@@ -1,7 +1,11 @@
 package com.example.tugas2_yazidnaufalhudzami_41522010078;
 
+import static androidx.core.widget.TextViewKt.addTextChangedListener;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -10,29 +14,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class loginorregister extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class register extends AppCompatActivity {
+    TextInputEditText textInputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_loginorregister);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    public void toLogin(View view) {
-        Intent login = new
-                Intent(loginorregister.this, login.class);
-        startActivity(login);
+    public void tologinorregister(View view) {
+        Intent loginorregister = new
+                Intent(register.this, loginorregister.class);
+        startActivity(loginorregister);
     }
-
-    public void toRegister(View view) {
-        Intent register = new
-                Intent(loginorregister.this, register.class);
-        startActivity(register);
+    public void toBeranda(View view) {
+        Intent beranda = new
+                Intent(register.this, beranda.class);
+        startActivity(beranda);
     }
 }
